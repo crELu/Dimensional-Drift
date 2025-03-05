7460f22ffdf50e335bb1cc43c6aca9995c572679
carolmeng9 <lakeharmony6854@gmail.com>
2025-03-05 01:59:34 -0500

    add shop ui assets, tweaked shop controls

--- Assets/Player/Shop/ShopManager.cs
+++ Assets/Player/Shop/ShopManager.cs
@@ -4,6 +4,8 @@ using TMPro;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
 public class ShopManager : MonoBehaviour
 {
@@ -12,34 +14,74 @@ public class ShopManager : MonoBehaviour
     [SerializeField] private PlayerManager playerManager;
     
     [Header("UI Elements")]
-    [SerializeField] private GameObject shopPanel;
+    [SerializeField] private GameObject shopInvParentPanel;
     [SerializeField] private TextMeshProUGUI intelText;
+    [SerializeField] private GameObject shopPage;
+    [SerializeField] private GameObject inventoryPage;
+    [SerializeField] private Button pageToggleButton;
+    [SerializeField] private TextMeshProUGUI toggleButtonText;
     [SerializeField] private Transform weaponSlotsContainer;
     [SerializeField] private GameObject weaponSlotPrefab;
     [SerializeField] private Transform shopItemsContainer;
     [SerializeField] private GameObject shopItemPrefab;
     
     [Header("Shop Items")]
-    [SerializeField] private List<ShopItem> availableItems = new();
+    [SerializeField] private List<ShopItemData> availableItems = new();
+
+    [Header("UI Navigation")]
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private GameObject firstShopItem;
+    private bool shopActive => shopInvParentPanel.activeSelf;
 
     private void Start()
     {
-        shopPanel.SetActive(false);
+        shopInvParentPanel.SetActive(false);
         InitializeWeaponSlots();
         InitializeShopItems();
-        UpdateUI();
+        
+        // Setup single toggle button
+        pageToggleButton.onClick.AddListener(() => SwitchToPage(!shopPage.activeSelf));
+        
+        // Start with shop inv page disabled
+        // SwitchToPage(true);
+        // UpdateUI();
+    }
+
+    private void Awake()
+    {
+        // Add some example shop items if the list is empty
+        if (availableItems.Count == 0)
+        {
+            // placeholder add core augment
+            var coreAugment = ScriptableObject.CreateInstance<CoreAugment>();
+            availableItems.Add(new ShopItemData
+            {
+                itemTitle = "Basic Core Augment",
+                description = "Enhances your weapon's core functionality",
+                cost = 50f,
+                icon = Resources.Load<Sprite>("Icons/CoreAugment"),
+                augment = coreAugment
+            });
+            
+            // maybe move list of all augments to a list of some sort
+        }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
+        if (InputSystem.actions.FindAction("Shop").WasPressedThisFrame() && PlayerManager.waveTimer > 0)
         {
             ToggleShop();
         }
         
-        if (shopPanel.activeSelf)
+        if (shopActive)
         {
             UpdateUI();
+            
+            if (InputSystem.actions.FindAction("Shop").WasPressedThisFrame())
+            {
+                ToggleShop();
+            }
         }
     }
 
@@ -99,26 +141,50 @@ public class ShopManager : MonoBehaviour
         }
     }
 
-    public void ToggleShop()
+    private void SwitchToPage(bool showShop)
     {
-        shopPanel.SetActive(!shopPanel.activeSelf);
-        if (shopPanel.activeSelf)
-        {
-            UpdateUI();
-        }
+        shopPage.SetActive(showShop);
+        inventoryPage.SetActive(!showShop);
+        toggleButtonText.text = showShop ? "Inventory" : "Shop";
+        UpdateUI();
     }
 
-    public bool PurchaseItem(ShopItem item)
+    public void ToggleShop()
     {
-        if (!item.CanPurchase(playerInventory)) return false;
+        shopInvParentPanel.SetActive(!shopActive);
+        
+        if (shopActive)
+        {
+            Time.timeScale = 0f;
+            playerInput.actions.FindActionMap("Player").Disable();
+            playerInput.actions.FindActionMap("UI").Enable();
+            UpdateWeaponSlots();
+            EventSystem.current.SetSelectedGameObject(firstShopItem);
+            SwitchToPage(false);  // Start with inventory page
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            playerInput.actions.FindActionMap("UI").Disable();
+            playerInput.actions.FindActionMap("Player").Enable();
+        }
+        
+        UpdateUI();
+    }
+
+    public bool PurchaseItem(ShopItemData item)
+    {
+        if (!item.augment.Requirements.All(req => 
+            req.exclusion ? !playerInventory.HasAugment(req.augment, req.minTier) 
+                         : playerInventory.HasAugment(req.augment, req.minTier))) return false;
         if (!playerInventory.SpendIntel(item.cost)) return false;
 
-        item.Apply(playerInventory, playerManager);
+        playerInventory.AddAugment(item.augment);
         UpdateUI();
         return true;
     }
     
-    public List<ShopItem> GetAvailableItems(ShopItemType? filterType = null)
+    public List<ShopItemData> GetAvailableItems(ShopItemType? filterType = null)
     {
         return filterType.HasValue 
             ? availableItems.Where(item => item.Type == filterType.Value).ToList() 
