7049569f504d12c231a65fd8feca9eedc83cbe7f
carolmeng9 <lakeharmony6854@gmail.com>
2025-03-10 15:24:51 -0400

    cleaned up shopmanager and playerinventory

--- Assets/Player/Shop/PlayerInventory.cs
+++ Assets/Player/Shop/PlayerInventory.cs
@@ -1,43 +1,37 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 
 public class PlayerInventory : MonoBehaviour
 {
-    [SerializeField] public int maxWeaponSlots = 3;
+    [SerializeField] private int maxWeaponSlots = 4;
     [SerializeField] private Transform weaponSlotsContainer;
+    [SerializeField] private TextMeshProUGUI intelText;
+    [SerializeField] private TextMeshProUGUI augmentsText;
+    [SerializeField] private TextMeshProUGUI statsText;
+
     public Dictionary<AugmentType, PlayerWeapon> equippedWeapons = new();
     public List<Augment> ownedAugments = new();
-    
+    public PlayerWeapon selectedWeapon;
     public float Intel { get; private set; }
 
-    public void Start()
+    private void Start()
     {
         Intel = 100;
+        InitializeDefaultWeapons();
+        UpdateUI();
     }
 
-    public bool CanEquipWeapon(AugmentType weaponType)
+    private void InitializeDefaultWeapons()
     {
-        return equippedWeapons.Count < maxWeaponSlots && !equippedWeapons.ContainsKey(weaponType);
-    }
-
-    public void EquipWeapon(PlayerWeapon weapon)
-    {
-        if (!CanEquipWeapon(weapon.WeaponType)) return;
-        equippedWeapons[weapon.WeaponType] = weapon;
-    }
-
-    public void UnequipWeapon(AugmentType weaponType)
-    {
-        if (equippedWeapons.ContainsKey(weaponType))
-        {
-            equippedWeapons.Remove(weaponType);
-        }
+        // Placeholder for default weapon initialization
     }
 
     public void AddAugment(Augment augment)
     {
         ownedAugments.Add(augment);
+        UpdateUI();
     }
 
     public bool HasAugment(Augment augment, int minTier)
@@ -45,9 +39,8 @@ public class PlayerInventory : MonoBehaviour
         return ownedAugments.Any(a => a.GetType() == augment.GetType() && a.Stacks >= minTier);
     }
 
-    // Helper method to get augments that can be applied to a specific weapon
-    public List<Augment> GetCompatibleAugments(PlayerWeapon weapon)
-    {
+    public List<Augment> GetCompatibleAugments(PlayerWeapon weapon) 
+    { 
         return ownedAugments.Where(augment => 
         {
             if (augment is CoreAugment coreAug)
@@ -59,9 +52,7 @@ public class PlayerInventory : MonoBehaviour
             return false;
         }).ToList();
     }
-
-    // Helper method to get character augments
-    public List<Augment> GetCharacterAugments()
+    public List<Augment> GetCharacterAugments() 
     {
         return ownedAugments.Where(augment => 
         {
@@ -73,17 +64,54 @@ public class PlayerInventory : MonoBehaviour
         }).ToList();
     }
 
-    public void AddIntel(float amount) => Intel += amount;
-
+    public void AddIntel(float amount) { Intel += amount; UpdateUI(); }
     public bool SpendIntel(float amount)
     {
         if (Intel >= amount)
         {
             Intel -= amount;
+            UpdateUI();
             return true;
         }
         return false;
     }
 
+    public void UpdateUI()
+    {
+        intelText.text = $"{Intel:F0}";
+        UpdateWeaponSlots();
+        UpdateAugmentsDisplay();
+    }
 
-} 
+    private void UpdateWeaponSlots()
+    {
+        var slots = weaponSlotsContainer.GetComponentsInChildren<WeaponSlotUI>();
+        foreach (var slot in slots)
+        {
+            if (equippedWeapons.TryGetValue(slot.slotType, out PlayerWeapon weapon))
+                slot.SetWeapon(weapon);
+            else
+                slot.Clear();
+        }
+    }
+
+    private void UpdateAugmentsDisplay()
+    {
+        if (selectedWeapon != null)
+        {
+            var weaponAugments = GetCompatibleAugments(selectedWeapon);
+            augmentsText.text = "Equipped Augments:\n" + string.Join("\n", weaponAugments.Select(a => a.name));
+        }
+        else
+        {
+            var charAugments = GetCharacterAugments();
+            augmentsText.text = "Character Augments:\n" + string.Join("\n", charAugments.Select(a => a.name));
+        }
+    }
+
+    public void SetSelectedWeapon(PlayerWeapon weapon)
+    {
+        selectedWeapon = weapon;
+        UpdateAugmentsDisplay();
+    }
+}
